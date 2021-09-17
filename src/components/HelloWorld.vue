e9d83de01cf6b614f8577fa0131cb57b119cc9d3
liuzhilin <17663969066@163.com>
2021-09-17 14:34:36 +0800

    测试

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,100 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+	<div
+		v-infinite-scroll="handleInfiniteOnLoad"
+		class="demo-infinite-container"
+		:infinite-scroll-disabled="busy"
+		:infinite-scroll-distance="10"
+	>
+		<a-table :columns="columns" :data-source="data" rowKey="email">
+			<div v-if="loading && !busy" class="demo-loading-container">
+				<a-spin />
+			</div>
+		</a-table>
+	</div>
 </template>
-
 <script>
+import reqwest from 'reqwest';
+import infiniteScroll from 'vue-infinite-scroll';
+const fakeDataUrl =
+	'https://randomuser.me/api/?results=5&inc=name,gender,email,nat&noinfo';
+const columns = [
+	{
+		title: 'Name',
+		dataIndex: 'name',
+		sorter: true,
+		width: '20%',
+		scopedSlots: { customRender: 'name' },
+	},
+	{
+		title: 'Gender',
+		dataIndex: 'gender',
+		filters: [
+			{ text: 'Male', value: 'male' },
+			{ text: 'Female', value: 'female' },
+		],
+		width: '20%',
+	},
+	{
+		title: 'Email',
+		dataIndex: 'email',
+	},
+];
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
+	directives: { infiniteScroll },
+	data() {
+		return {
+			data: [],
+			loading: false,
+			busy: false,
+			columns,
+		};
+	},
+	beforeMount() {
+		this.fetchData((res) => {
+			this.data = res.results;
+		});
+	},
+	methods: {
+		fetchData(callback) {
+			reqwest({
+				url: fakeDataUrl,
+				type: 'json',
+				method: 'get',
+				contentType: 'application/json',
+				success: (res) => {
+					callback(res);
+				},
+			});
+		},
+		handleInfiniteOnLoad() {
+			const data = this.data;
+			this.loading = true;
+			if (data.length > 14) {
+				this.$message.warning('Infinite List loaded all');
+				this.busy = true;
+				this.loading = false;
+				return;
+			}
+			this.fetchData((res) => {
+				this.data = data.concat(res.results);
+				this.loading = false;
+			});
+		},
+	},
+};
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
+<style>
+.demo-infinite-container {
+	border: 1px solid #e8e8e8;
+	border-radius: 4px;
+	overflow: auto;
+	padding: 8px 24px;
+	height: 300px;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+.demo-loading-container {
+	position: absolute;
+	bottom: 40px;
+	width: 100%;
+	text-align: center;
 }
 </style>
