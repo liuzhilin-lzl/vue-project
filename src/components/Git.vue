1e68f267e7182353b971f237accb195682ceaad3
刘志麟521 <56372072+yaoyutong521@users.noreply.github.com>
2021-11-26 08:08:35 +0800

    添加组件

--- src/components/Git.vue
+++ src/components/Git.vue
@@ -1,17 +1,27 @@
 <!--
  * @Author: your name
- * @Date: 2021-10-17 12:42:10
- * @LastEditTime: 2021-10-17 13:17:11
+ * @Date: 2021-10-17 13:16:46
+ * @LastEditTime: 2021-10-17 14:40:42
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /vue-project/src/components/Git.vue
 -->
 <template>
-	<div>测试Merge git 怎么回事 设么情况</div>
+	<div class="div">
+		div 测试Merge git 怎么回事 设么情况 好了
+		<div>
+			<div>
+				<div>di</div>
+			</div>
+		</div>
+	</div>
 </template>
 <script>
 export default {};
 </script>
 <style scoped>
+.div {
+	color: rgb(255, 0, 0);
+}
 </style>>
 	
